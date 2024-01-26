53723e04d2c4e1095662b7e566ca8b88ed357d2e
Rachel Galdino <rachel.galdino@outlook.com>
2024-01-26 16:03:25 -0300

    added local storage

---
+++ src/script/init.js
@@ -0,0 +1,8 @@
+const bodyTag = document.querySelector('body');
+
+const theme = window.localStorage.getItem('theme');
+
+if (theme === 'dark') {
+  bodyTag.classList.add('dark');
+  bodyTag.classList.remove('light');
+}
--- src/script/theme.js
+++ src/script/theme.js
@@ -6,6 +6,8 @@ themeButton.addEventListener('click', function () {
   body.classList.toggle('dark');
   body.classList.toggle('light');
 
+  window.localStorage.setItem('theme', body.classList.contains('dark') ? 'dark' : 'light'); // if body.classList.contains('dark') is true, then 'dark' is returned, otherwise 'light' is returned
+
   icon.classList.toggle('fa-moon');
   icon.classList.toggle('fa-sun');
 });
