06248125cef2add9134b5c531a7cb132e6b806d4
Rachel Galdino <rachel.galdino@outlook.com>
2024-01-26 16:55:56 -0300

    started adding history

--- src/script/calc.js
+++ src/script/calc.js
@@ -3,6 +3,18 @@ let result = document.getElementById('calcResult');
 
 document.addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
-    result.innerHTML = eval(input.value); // what eval does is it takes a string and evaluates it as if it were a JavaScript expression
+    const resultCalc = eval(input.value);
+    result.innerHTML = resultCalc; // what eval does is it takes a string and evaluates it as if it were a JavaScript expression
+
+    const history = {
+      input: input.value,
+      result: resultCalc
+    };
+
+    const previousValues = JSON.parse(window.localStorage.getItem('history')) || [];
+
+    previousValues.push(history);
+    window.localStorage.setItem('history', JSON.stringify(previousValues));
   }
 });
+
